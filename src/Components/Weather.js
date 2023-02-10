7c354a6d447bd57d8ce60e78659a6480e3406e2f
Taryn King <marshaeking@gmail.com>
2023-02-10 10:14:55 -0600

    refactor: add logic to request and display weather details in Weather component

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,31 +1,54 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useSelector } from "react-redux";
+import { selectDisplay } from "../redux/slices/displayCountrySlice";
 
 const Weather = () => {
     const [weather, setWeather] = useState();
+    const currentDisplay = useSelector(selectDisplay);
 
-    // ------------------------------------
-    // PASTE RAPIDAPI CODE SNIPPET IN A USEEFFECT HERE
-    // ------------------------------------
+    const latitude = currentDisplay.latlng[0];
+    const longitude = currentDisplay.latlng[1];
+
+    useEffect(() => {
+
+        const options = {
+            method: 'GET',
+            url: 'https://weatherapi-com.p.rapidapi.com/current.json',
+            params: { q: `${latitude},${longitude}` },
+            headers: {
+                'X-RapidAPI-Key': '2e1eb485a8msh58c29bd1e6d9e4dp1963a4jsn293239bdef7a',
+                'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+            }
+        };
+
+        axios.request(options).then(response => {
+            setWeather(response.data);
+        }).catch(error => {
+            console.error(error);
+        });
+    }, [])
 
     return (
         <div>
             <table className="overview-table">
-                <tr>
-                    <td>Conditions: </td>
-                </tr>
-                <tr>
-                    <td>Temperature: </td>
-                </tr>
-                <tr>
-                    <td>Feels Like: </td>
-                </tr>
-                <tr>
-                    <td>Humidity: </td>
-                </tr>
-                <tr>
-                    <td>Wind Speed: </td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <td>Conditions: {weather.current.condition.text}</td>
+                    </tr>
+                    <tr>
+                        <td>Temperature: {weather.current.temp_c} C / {weather.current.temp_f} F</td>
+                    </tr>
+                    <tr>
+                        <td>Feels Like: {weather.current.feelslike_c} C / {weather.current.feelslike_f} F</td>
+                    </tr>
+                    <tr>
+                        <td>Humidity: {weather.current.humidity}%</td>
+                    </tr>
+                    <tr>
+                        <td>Wind Speed: {weather.current.wind_mph} mph / {weather.current.wind_kph} kph</td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     );
