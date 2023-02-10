fc88add8e58586164bef5016373a532e3e900704
Taryn King <marshaeking@gmail.com>
2023-02-10 08:33:14 -0600

    refactor: add logic to render details in Symbols component

--- src/Components/Symbols.js
+++ src/Components/Symbols.js
@@ -1,6 +1,20 @@
+import { useSelector } from 'react-redux';
+import { selectDisplay } from "../redux/slices/displayCountrySlice";
+
 const Symbols = () => {
+    const currentDisplay = useSelector(selectDisplay);
+
     return (
-        <div>symbols</div>
+        <div className="symbols">
+            <div className="stack">
+                <h2>Flag of {currentDisplay.name.common}</h2>
+                {currentDisplay.flags.png ? (<img src={currentDisplay.flags.png} alt={`Country flag of ${currentDisplay.name.official}`} />) : ('No data')}
+            </div>
+            <div className="stack">
+                <h2>Coat of Arms of {currentDisplay.name.common}</h2>
+                {currentDisplay.coatOfArms.png ? (<img src={currentDisplay.coatOfArms.png} alt={`Coat of Arms of ${currentDisplay.name.official}`} />) : ('No data')}
+            </div>
+        </div>
     );
 }
 
